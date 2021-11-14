eebd640a40f48981cebd68aa38accace356a7370
ph0nik <phonik83@gmail.com>
2021-11-14 14:57:44 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    /*
+    * TODO
+    *  1. scan files
+    *  2. filter file names
+    * 3. add matching files to db
+    * 4. send notification
+    * 5. select file to search
+    * 6. send search request for file name
+    * 7. prompt user with suggestions
+    * 8. make selection and create file link with given data
+    *
+    * */
+
+}
